b264b7357ad9b13b8d92c3c7afe416a189548294
DaveBlueFrontier <dave.probert@bluefrontier.co.uk>
2016-06-13 10:39:58 +0100

    Replacement of DB Migrate for user timeout

--- db/migrate/20160610101601_add_timeout_in_seconds_to_users.rb
+++ db/migrate/20160610101601_add_timeout_in_seconds_to_users.rb
@@ -1,4 +1,4 @@
-class AddTimoutInSecondsToUsers < ActiveRecord::Migration
+class AddTimeoutInSecondsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :timeout_in_seconds, :integer, default: 180
   end
