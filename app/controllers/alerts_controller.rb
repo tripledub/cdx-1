7c42d9167c6dcae5513bea99e6bc8c21eb85e8b5
DaveBlueFrontier <dave.probert@bluefrontier.co.uk>
2016-06-15 17:05:02 +0100

    Alert Groups Display Fix

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -5,4 +5,10 @@ class AlertsController < ApplicationController
     head :forbidden unless has_access_to_test_results_index?
   end
 
+  def index
+    @alerts = current_user.alerts
+    @total = @alerts.count
+    respond_with @total
+  end
+
 end
