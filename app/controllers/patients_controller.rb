33f4f8171ae52e7e99170f7448972973ce3e62bc
Jorge Alvarez <ja@conferize.com>
2016-06-17 15:36:01 +0200

    Story #1824 new episode summary with edit button.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -35,7 +35,6 @@ class PatientsController < ApplicationController
     @patient = Patient.find(params[:id])
     @patient_json = Jbuilder.new { |json| @patient.as_json_card(json) }.attributes!
     return unless authorize_resource(@patient, READ_PATIENT)
-    @can_edit = has_access?(@patient, UPDATE_PATIENT)
     @encounters = @patient.encounters.order(start_time: :desc)
   end
 
