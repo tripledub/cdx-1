f59b3bd0dfa5a0e28801550117fc40e1f9825956
Jorge Alvarez <ja@conferize.com>
2016-06-20 16:16:56 +0200

    Story #1829 User must be able to sort audit table.

--- lib/presenters/comments.rb
+++ lib/presenters/comments.rb
@@ -3,6 +3,7 @@ class Presenters::Comments
     def patient_view(comments)
       comments.map do |comment|
         {
+          id:           comment.uuid,
           comment_date: comment.commented_on.strftime(I18n.t('date.input_format.pattern')),
           commenter:    comment.user.full_name,
           title:        comment.description,
