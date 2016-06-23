6ea7d10be8d19331f68b538e51a3e940a63f703f
Jorge Alvarez <ja@conferize.com>
2016-06-23 13:28:39 +0200

    Show comment details.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,26 +2,31 @@ class CommentsController < ApplicationController
   respond_to :html
   respond_to :json, only: [:index]
 
-  before_filter :find_patient
-  before_filter :check_permissions, only: [:new, :create]
+  expose(:patient) { find_patient }
 
-  expose(:comments, scope: -> { @patient.comments })
+  expose(:comments, scope: -> { patient.comments })
+
+  expose(:comment, scope: -> { patient.comments }, attributes: :comment_params)
+
+  before_filter :check_permissions, only: [:new, :create]
 
   def index
     render json: Presenters::Comments.patient_view(comments.joins(:user).order(set_order_from_params).limit(30).offset(params[:page] || 0))
   end
 
+  def show
+  end
+
   def new
-    @comment = @patient.comments.new
   end
 
   def create
-    @comment              = @patient.comments.new(comment_params)
-    @comment.user         = current_user
-    @comment.commented_on = Extras::Dates::Format.string_to_pattern(params[:comment][:commented_on])
+    comment.patient      = patient
+    comment.user         = current_user
+    comment.commented_on = Extras::Dates::Format.string_to_pattern(params[:comment][:commented_on])
 
-    if @comment.save_and_audit(current_user, 'New comment added', @comment.comment)
-      redirect_to patient_path(@patient), notice: 'Comment was successfully created.'
+    if comment.save_and_audit(current_user, 'New comment added', comment.comment)
+      redirect_to patient_path(patient), notice: 'Comment was successfully created.'
     else
       render action: 'new'
     end
@@ -30,15 +35,15 @@ class CommentsController < ApplicationController
   protected
 
   def find_patient
-    @patient = Patient.where(institution: @navigation_context.institution, id: params[:patient_id]).first
+    Patient.where(institution: @navigation_context.institution, id: params[:patient_id]).first
   end
 
   def comment_params
-    params.require(:comment).permit(:commented_on, :description, :comment)
+    params.require(:comment).permit(:commented_on, :description, :comment, :image)
   end
 
   def check_permissions
-    redirect_to(patient_path(@patient), error: "You can't add comments to this patient") unless has_access?(@patient, UPDATE_PATIENT)
+    redirect_to(patient_path(patient), error: "You can't add comments to this patient") unless has_access?(patient, UPDATE_PATIENT)
   end
 
   def set_order_from_params
