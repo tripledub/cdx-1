ee3c108a08ec58577b0aa65dd1306eb3f57c6106
Jorge Alvarez <ja@conferize.com>
2016-06-23 13:52:18 +0200

    Add image attachment validation.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,4 +8,6 @@ class Comment < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :description, :commented_on, :patient_id, :user_id
+
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
