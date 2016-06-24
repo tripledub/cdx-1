361419e5ff05d09de07f976ab054cb304de5de35
Jorge Alvarez <ja@conferize.com>
2016-06-24 14:54:36 +0200

    Remove decent exposure.

--- app/controllers/audit_updates_controller.rb
+++ app/controllers/audit_updates_controller.rb
@@ -3,17 +3,15 @@ class AuditUpdatesController < ApplicationController
 
   before_filter :find_patient_and_log
 
-  expose(:audit_updates, scope: -> { @audit_log.audit_updates })
-
   def index
-    render json: Presenters::AuditUpdates.patient_view(audit_updates.order(set_order_from_params).limit(30).offset(params[:page] || 0))
+    render json: Presenters::AuditUpdates.patient_view(@audit_log.audit_updates.order(set_order_from_params).limit(30).offset(params[:page] || 0))
   end
 
   protected
 
   def find_patient_and_log
     patient = Patient.where(institution: @navigation_context.institution, id: params[:patient_id]).first
-    @audit_log = patient.audit_logs.where(id: params[:patient_log_id])
+    @audit_log = patient.audit_logs.where(id: params[:patient_log_id]).first
   end
 
   def set_order_from_params
