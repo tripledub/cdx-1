8559a3582bec13441f29a674c4935040c4ce2827
Jorge Alvarez <ja@conferize.com>
2016-06-29 10:15:29 +0200

    Fix failing tests.

--- db/schema.rb
+++ db/schema.rb
@@ -136,7 +136,7 @@ ActiveRecord::Schema.define(version: 20160627152542) do
   add_index "audit_updates", ["uuid"], name: "index_audit_updates_on_uuid", using: :btree
 
   create_table "comments", force: :cascade do |t|
-    t.date     "commented_on",                     default: '2016-06-21'
+    t.date     "commented_on",                     default: '2016-06-15'
     t.text     "comment",            limit: 65535
     t.string   "description",        limit: 255
     t.string   "uuid",               limit: 255
@@ -665,7 +665,6 @@ ActiveRecord::Schema.define(version: 20160627152542) do
     t.boolean  "is_active",                                  default: true
     t.string   "telephone",                      limit: 255
     t.boolean  "sidebar_open",                               default: true
-    t.integer  "timeout_in_seconds",             limit: 4,   default: 180
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
