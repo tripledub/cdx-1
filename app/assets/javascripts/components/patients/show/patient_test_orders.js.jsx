d524d112193afbe449a8ebe51ac80daf98c52532
Jorge Alvarez <ja@conferize.com>
2016-06-30 10:18:47 +0200

    Hide table header if no content.

--- app/assets/javascripts/components/patients/show/patient_test_orders.js.jsx
+++ app/assets/javascripts/components/patients/show/patient_test_orders.js.jsx
@@ -41,22 +41,24 @@ var PatientTestOrders = React.createClass({
 
     return (
       <div className="row">
-        {this.state.patientTestOrders.length < 1 ? <LoadingResults loadingMessage={this.state.loadingMessage} /> : null}
-        <table className="patient-test-orders">
-          <thead>
-            <tr>
-              <th><a href="#" onClick={this.getTestOrders.bind(null, 'site')}>Site</a></th>
-              <th><a href="#" onClick={this.getTestOrders.bind(null, 'orderId')}>Order ID</a></th>
-              <th><a href="#" onClick={this.getTestOrders.bind(null, 'requester')}>Requested By</a></th>
-              <th><a href="#" onClick={this.getTestOrders.bind(null, 'date')}>Request Date</a></th>
-              <th><a href="#" onClick={this.getTestOrders.bind(null, 'dueDate')}>Due Date</a></th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {rows}
-          </tbody>
-        </table>
+        {
+          this.state.patientTestOrders.length < 1 ? <LoadingResults loadingMessage={this.state.loadingMessage} /> :
+          <table className="patient-test-orders">
+            <thead>
+              <tr>
+                <th><a href="#" onClick={this.getTestOrders.bind(null, 'site')}>Site</a></th>
+                <th><a href="#" onClick={this.getTestOrders.bind(null, 'orderId')}>Order ID</a></th>
+                <th><a href="#" onClick={this.getTestOrders.bind(null, 'requester')}>Requested By</a></th>
+                <th><a href="#" onClick={this.getTestOrders.bind(null, 'date')}>Request Date</a></th>
+                <th><a href="#" onClick={this.getTestOrders.bind(null, 'dueDate')}>Due Date</a></th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {rows}
+            </tbody>
+          </table>
+        }
       </div>
     );
   }
