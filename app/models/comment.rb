12ae01850d747b62f86b89867ff03449b683ad14
Jorge Alvarez <ja@conferize.com>
2016-06-30 12:59:07 +0200

    Fixed tests.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,14 +10,4 @@ class Comment < ActiveRecord::Base
   validates_presence_of :description, :commented_on, :patient_id, :user_id
 
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
-
-  before_validation :add_default_date
-
-  protected
-
-  def add_default_date
-    return if commented_on.present?
-
-    self.commented_on = Date.today
-  end
 end
