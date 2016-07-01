7fdf7d4f1e906f5555453dec1bc52e1f6939da20
Jorge Alvarez <ja@conferize.com>
2016-07-01 15:07:25 +0200

    Fixed outdated helper.

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -1,5 +1,4 @@
 module EpisodesHelper
-
   def outcome_options
     opts = Episode.treatment_outcome_options.inject([]) do |arr, obj|
       arr << obj.marshal_dump
@@ -8,7 +7,7 @@ module EpisodesHelper
   end
 
   def diagnosis_name(diagnosis)
-    return  unless diagnosis.present?
+    return unless diagnosis.present?
     Episode.diagnosis_options.select{ |option| option.id == diagnosis.to_sym }.first.name
   end
 
@@ -34,42 +33,6 @@ module EpisodesHelper
 
   def treatment_outcome_name(treatment_outcome)
     return unless treatment_outcome.present?
-    Episode.treatment_outcome_options.select{ |option| option.id == treatment_outcome.to_sym }.first.name
-  end
-
-  def diagnosis_name(diagnosis)
-    return unless diagnosis
-
-    Episode.diagnosis_options.select{ |option| option.id == diagnosis.to_sym }.first.name
-  end
-
-  def hiv_status_name(hiv_status)
-    return unless hiv_status
-
-    Episode.hiv_status_options.select{ |option| option.id == hiv_status.to_sym }.first.name
-  end
-
-  def history_options_name(initial_history)
-    return unless initial_history
-
-    Episode.initial_history_options.select{ |option| option.id == initial_history.to_sym }.first.name
-  end
-
-  def previous_history_options_name(previous_history)
-    return unless previous_history
-
-    Episode.previous_history_options.select{ |option| option.id == previous_history.to_sym }.first.name
-  end
-
-  def drug_resistance_name(drug_resistance)
-    return unless drug_resistance
-
-    Episode.drug_resistance_options.select{ |option| option.id == drug_resistance.to_sym }.first.name
-  end
-
-  def treatment_outcome_name(treatment_outcome)
-    return unless treatment_outcome
-
     Episode.treatment_outcome_options.select{ |option| option.id == treatment_outcome.to_sym }.first.name
   end
 end
