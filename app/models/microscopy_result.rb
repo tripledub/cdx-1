73cce6882fb7fdb80220281a2016c25458f5434b
Jorge Alvarez <ja@conferize.com>
2016-07-06 13:43:18 +0200

    Fix wrong selecte field.

--- app/models/microscopy_result.rb
+++ app/models/microscopy_result.rb
@@ -1,7 +1,7 @@
 class MicroscopyResult < PatientResult
   validates_presence_of  :sample_collected_on, :examined_by, :result_on, :specimen_type, :serial_number, :appearance
   validates_inclusion_of :results_negative, :results_1to9, :results_1plus, :results_2plus, :results_3plus, in: [true, false]
-  validates_inclusion_of :specimen_type, in: ['blood', 'mucopurulent', 'saliva']
+  validates_inclusion_of :appearance, in: ['blood', 'mucopurulent', 'saliva']
 
   class << self
     def visual_appearance_options
