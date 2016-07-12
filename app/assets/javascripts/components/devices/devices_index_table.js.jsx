fa668e8ce0a7785fa6df448e09a5fcd0f8c8f401
Jorge Alvarez <ja@conferize.com>
2016-07-12 15:22:45 +0200

    Move section content out of the table.

--- app/assets/javascripts/components/devices/devices_index_table.js.jsx
+++ app/assets/javascripts/components/devices/devices_index_table.js.jsx
@@ -30,42 +30,31 @@ var DevicesIndexTable = React.createClass({
   },
 
   render: function() {
+    var rows           = [];
     var sortableHeader = function (title, field) {
       return <SortableColumnHeader title={title} field={field} orderBy={this.props.orderBy} />
     }.bind(this);
 
+    this.props.devices.forEach(
+      function(device) {
+        rows.push(<DeviceResultRow key={device.id} device={device} />);
+      }
+    );
+
     return (
-      <section className="row">
-        <div className="small-12 columns box">
-          <header>
-            <img src="/assets/img/test-tube-rack-5b3580a9d8fc54b5cf7bd4ca808fb86c.png" alt="Test tube rack" />
-            <h3>{this.props.title}</h3>
-            { this.props.downloadCsvPath ? (
-              <span className="table-actions">
-                <a href={this.props.downloadCsvPath} title="Download CSV">
-                  <span className="icon-download icon-gray"></span>
-                </a>
-              </span>) : null }
-          </header>
-          <div className="box-content">
-            <table className="table" cellPadding="0" cellSpacing="0" data-resizable-columns-id="devices-table">
-              <thead>
-                <tr>
-                  {sortableHeader("Name", "devices.name")}
-                  {sortableHeader("Manufacturer", "institutions.name")}
-                  {sortableHeader("Model", "device_models.name")}
-                  {sortableHeader("Site", "sites.name")}
-                </tr>
-              </thead>
-              <tbody>
-                {this.props.devices.map(function(device) {
-                   return <DeviceResultRow key={device.id} device={device} />;
-                }.bind(this))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </section>
+      <table className="table" cellPadding="0" cellSpacing="0" data-resizable-columns-id="devices-table">
+        <thead>
+          <tr>
+            {sortableHeader("Name", "devices.name")}
+            {sortableHeader("Manufacturer", "institutions.name")}
+            {sortableHeader("Model", "device_models.name")}
+            {sortableHeader("Site", "sites.name")}
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
     );
   }
 });
