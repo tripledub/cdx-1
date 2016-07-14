d692a100d0cf29148f969a2e1eb92169e913b42c
Stewart Campbell <stewartcuk@gmail.com>
2016-07-14 19:11:04 +0100

    Add database cleaning to specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,6 +35,17 @@ class ActiveRecord::Reflection::AssociationReflection
 end
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
   config.render_views
   config.infer_spec_type_from_file_location!
 
