94e9b4dd92b09cf2993ac73f2bdbe32958ecb629
Jorge Alvarez <ja@conferize.com>
2016-07-19 12:57:56 +0200

    Fix wrong field.

--- db/migrate/20160719090046_add_birth_date_and_nickname_to_patient.rb
+++ db/migrate/20160719090046_add_birth_date_and_nickname_to_patient.rb
@@ -1,7 +1,7 @@
 class AddBirthDateAndNicknameToPatient < ActiveRecord::Migration
   def change
     add_column :patients, :nickname, :string
-    add_column :patients, :birth_date_on_on, :date
-    add_index  :patients, :birth_date_on_on
+    add_column :patients, :birth_date_on, :date
+    add_index  :patients, :birth_date_on
   end
 end
