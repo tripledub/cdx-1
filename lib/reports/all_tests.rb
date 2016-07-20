43b2d39af00aa44b1e665e7358fde6d08b829877
Jorge Alvarez <ja@conferize.com>
2016-07-20 20:12:11 +0200

    Merge device results with manual results.

--- lib/reports/all_tests.rb
+++ lib/reports/all_tests.rb
@@ -17,6 +17,7 @@ module Reports
 
     def generate_chart
       results = process
+      merge_with_manual_results(results)
       if results.number_of_months > 1
         sorted_data = results.sort_by_month.data
       else
@@ -86,6 +87,31 @@ module Reports
           dataPoints: sorted_data.map { |data_point| { label: data_point[:label], y: data_point[:values][1] } }
         }
       ]
+    end
+
+    def merge_with_manual_results(test_results)
+      filter       = test_results.filter
+      since_day    = filter['since'] + ' 00:00'
+      until_day    = (filter['until'] || Date.today.strftime("%Y-%m-%d")) + ' 23:59'
+      manual_query = PatientResult.where('patient_results.type != "TestResult"').joins('LEFT OUTER JOIN requested_tests ON requested_tests.id = patient_results.requested_test_id')
+      manual_query.joins('LEFT OUTER JOIN encounters ON encounters.id = requested_tests.encounter_id')
+      manual_query.joins('LEFT OUTER JOIN institutions ON institutions.id = encounters.institutions_id')
+      manual_query.joins('LEFT OUTER JOIN sites ON sites.id = encounters.site_id')
+      manual_query.where({ 'institutions.uuid' => filter['institution.uuid'] }) if filter['institution.uuid']
+      manual_query.where({ 'sites.uuid'        => filter['site.uuid'] })        if filter['site.uuid']
+      manual_results = manual_query.where({ 'patient_results.created_at' => since_day..until_day }).where(options).group('date(patient_results.created_at)').count
+
+      merge_results(test_results, manual_results)
+    end
+
+    def merge_results(test_results, manual_results)
+      test_results.results['tests'].each do |auto_test|
+        manual_results.each do |key, value|
+          auto_test['count'] += value if auto_test['test.status'] == 'success' && auto_test['test.start_time'] == key.strftime("%Y-%m-%d")
+        end
+      end
+
+      test_results
     end
   end
 end
