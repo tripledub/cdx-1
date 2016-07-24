dfe493c52a381766919e9e0ba4410f11338ee112
Stewart Campbell <stewartcuk@gmail.com>
2016-07-24 18:51:18 +0100

    Shows all of form on page load

--- app/assets/javascripts/components/encounter_new.js.jsx
+++ app/assets/javascripts/components/encounter_new.js.jsx
@@ -62,7 +62,8 @@ var EncounterNew = React.createClass({
 
   render: function() {
     var sitesUrl = URI("/encounters/sites").query({context: this.props.context.institution.uuid});
-    var siteSelect = <SiteSelect onChange={this.setSite} url={sitesUrl} fieldLabel='Requesting' defaultSiteUuid={this.state.defaultSiteUuid} allow_manual_entry_callback={this.allow_manual_entry_callback} />;
+    var siteUuid = this.props.context.site ? this.props.context.site.uuid : null
+    var siteSelect = <SiteSelect onChange={this.setSite} url={sitesUrl} fieldLabel='Requesting' defaultSiteUuid={siteUuid} allow_manual_entry_callback={this.allow_manual_entry_callback} />;
     var performingSiteSelect = <SiteSelect onChange={this.setPerformingSite} url={sitesUrl} fieldLabel='Performing' defaultSiteUuid={_.get(this.props.context.performingsite, 'uuid')} />;
 
     if (this.state.encounter.site == null) {
