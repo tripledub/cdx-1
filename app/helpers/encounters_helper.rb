0a28c77bbb228b7d7230780314f19db8e31693b3
Stewart Campbell <stewartcuk@gmail.com>
2016-07-24 20:17:08 +0100

    Ensure a site is selected on drop down

--- app/helpers/encounters_helper.rb
+++ app/helpers/encounters_helper.rb
@@ -1,10 +1,13 @@
 module EncountersHelper
   def encounter_context(navigation_context)
-    unless navigation_context.site
-      institution = navigation_context.institution
-      site = institution.sites.first
-      navigation_context.site = site
-    end
-    navigation_context.to_hash
+    context = navigation_context.to_hash
+    context[:site] ||= site(navigation_context)
+    context
+  end
+
+  def site(navigation_context)
+    sites = navigation_context.institution.sites
+    site = sites.sort_by(&:name).first
+    return { name: site.name, uuid: site.uuid } if site
   end
 end
