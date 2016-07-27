76e2906f2e6fd63dd41437950240799d651deedf
Jorge Alvarez <ja@conferize.com>
2016-07-27 14:03:56 +0200

    Add middleware class to update status.

--- spec/models/patient_result_spec.rb
+++ spec/models/patient_result_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
 describe PatientResult do
+  let(:test_result)         { MicroscopyResult.make }
+
   context "validations" do
     it { should belong_to(:requested_test) }
   end
-    
+
+  context 'after_save' do
+    it 'should update status' do
+      expect(TestStatus).to receive(:change_status).with(test_result)
+
+      test_result.save
+    end
+  end
 end
