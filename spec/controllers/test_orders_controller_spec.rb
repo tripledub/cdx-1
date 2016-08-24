72fc1327284fad0f62025cb6b034ba7bb22be8c8
Jorge Alvarez <ja@conferize.com>
2016-08-24 18:10:32 +0200

    Update Test Order search.

--- spec/controllers/test_orders_controller_spec.rb
+++ spec/controllers/test_orders_controller_spec.rb
@@ -5,9 +5,17 @@ describe TestOrdersController, elasticsearch: true do
   let(:user)           { User.make }
   let!(:institution)   { user.institutions.make }
   let(:site)           { Site.make institution: institution }
+  let(:site2)          { Site.make institution: institution }
   let(:patient)        { Patient.make institution: institution }
-  let(:encounters)     { 7.times { Encounter.make institution: institution, site: site, patient: patient, start_time: 3.days.ago.to_s, testdue_date: 1.day.from_now.to_s, status: rand(0..2) } }
-  let(:default_params) { { context: institution.uuid } }
+  let!(:encounters)    {
+    7.times {
+      encounter = Encounter.make institution: institution, site: site, patient: patient, start_time: 3.days.ago.strftime("%Y-%m-%d"), testdue_date: 1.day.from_now.strftime("%Y-%m-%d"), status: rand(0..2)
+      EncounterIndexer.new(encounter).index(true)
+    }
+    encounter = Encounter.make institution: institution, site: site, performing_site_id: site2.id, patient: patient, start_time: 3.days.ago.strftime("%Y-%m-%d"), testdue_date: 1.day.from_now.strftime("%Y-%m-%d"), status: rand(0..2)
+    EncounterIndexer.new(encounter).index(true)
+  }
+  let(:default_params) { { context: site.uuid } }
 
   context 'a logged in user' do
     before(:each) do
@@ -16,10 +24,36 @@ describe TestOrdersController, elasticsearch: true do
 
     describe 'index' do
       context 'format html' do
-        it 'should display the right template' do
-          get :index
+        context 'when context is site' do
+          before :each do
+            default_params
+            get :index
+          end
 
-          expect(response).to render_template('index')
+          it 'should display the right template' do
+            expect(response).to render_template('index')
+          end
+
+          it 'should display the test orders' do
+            expect(assigns(:total)).to eq(8)
+          end
+        end
+
+        context 'when context is site2' do
+          let(:default_params) { { context: site2.uuid } }
+
+          before :each do
+            default_params
+            get :index
+          end
+
+          it 'should display the right template' do
+            expect(response).to render_template('index')
+          end
+
+          it 'should display the test orders this site is performing' do
+            expect(assigns(:total)).to eq(1)
+          end
         end
       end
 
@@ -29,10 +63,6 @@ describe TestOrdersController, elasticsearch: true do
         end
 
         it 'should be a csv response' do
-          expect(response.header["Content-Type"]).to eq('text/csv')
-        end
-
-        it 'should return data as csv' do
           expect(response.header["Content-Type"]).to eq('text/csv')
         end
       end
