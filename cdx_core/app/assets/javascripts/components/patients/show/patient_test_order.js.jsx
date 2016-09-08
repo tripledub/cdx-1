e1754ab4a5f71c98be0ac90e69857853ad0a29c5
Stewart Campbell <stewartcuk@gmail.com>
2016-09-08 18:48:53 +0100

    Adding :batch_id to test orders table

--- cdx_core/app/assets/javascripts/components/patients/show/patient_test_order.js.jsx
+++ cdx_core/app/assets/javascripts/components/patients/show/patient_test_order.js.jsx
@@ -8,7 +8,7 @@ var PatientTestOrder = React.createClass({
       <tr onClick={this.navigateTo.bind(this, this.props.patientTestOrder.viewLink)}>
         <td>{this.props.patientTestOrder.siteName}</td>
         <td>{this.props.patientTestOrder.performingSiteName}</td>
-        <td>{this.props.patientTestOrder.id}</td>
+        <td>{this.props.patientTestOrder.batch_id}</td>
         <td>{this.props.patientTestOrder.requester}</td>
         <td>{this.props.patientTestOrder.requestDate}</td>
         <td className={this.props.patientTestOrder._highlight_overdue}>{this.props.patientTestOrder.dueDate}</td>
