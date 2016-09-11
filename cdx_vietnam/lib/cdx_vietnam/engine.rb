2825ebe6aaaa4ded52458210b48be01547282127
Stewart Campbell <stewartcuk@gmail.com>
2016-09-11 15:12:11 +0100

    Adds ActiveSupport Decorators

--- cdx_vietnam/lib/cdx_vietnam/engine.rb
+++ cdx_vietnam/lib/cdx_vietnam/engine.rb
@@ -3,6 +3,10 @@ module CdxVietnam
     config.autoload_paths += %W(#{CdxVietnam::Engine.root}/lib)
     config.i18n.load_path += Dir[File.join(CdxVietnam::Engine.root, 'config', 'locales', '**', '*.{rb,yml}')]
 
+    initializer :set_decorator_paths, before: :load_environment_hook do |app|
+      ActiveSupportDecorators.paths << File.join(config.root, 'app/**')
+    end
+
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
