81ffdfecd242baab46e1609012ce99ad5bcec9f3
Jorge Alvarez <jorge@alvareznavarro.es>
2016-09-13 10:32:59 +0200

    Fix typo.

--- cdx_core/app/controllers/application_controller.rb
+++ cdx_core/app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = current_user.try(:locale) || I18n.default_locale
-    I18n.locale = params[:language] ? params[:language].to_sym || :en
+    I18n.locale = params[:language].present? ? params[:language].to_sym : :en
     @localization_helper = LocalizationHelper.new(current_user.try(:time_zone), I18n.locale,
       current_user.try(:timestamps_in_device_time_zone))
   end
