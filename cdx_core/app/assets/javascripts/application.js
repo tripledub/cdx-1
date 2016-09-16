f92084110f07e50de5fabf4a7944167dbc87017d
LienPTB <lienptb@elarion.com>
2016-09-16 17:13:47 +0700

    Fix review

--- cdx_core/app/assets/javascripts/application.js
+++ cdx_core/app/assets/javascripts/application.js
@@ -44,6 +44,7 @@
 //= require jquery-ui.min
 //= require jquery.comiseo.daterangepicker.min
 //= require jquery.bettertabs.min
+//= menu
 
 Turbolinks.enableProgressBar()
 
@@ -225,30 +226,6 @@ $(document).ready(function(){
     // We want to set overflow visible after the expand animation has completed
     $(".custom_filters").toggle();
   });
-
-  $(".icon-user").on('click', function(){
-    $(".user").toggleClass("show");
-  });
-  
-  $('body').click(function(e){
-    if (!e.target.matches('.icon-user')){
-      if($('.user').hasClass('show')){
-        $('.user').removeClass('show');
-      }
-    }
-  });
-  
-  $('.select_lang').on('change', function(){
-    var lang = $('.select_lang').val();
-    $.ajax({
-      url: '/users/change_language',
-      data: {language: lang},
-      success: function(){
-        location.reload();
-      }
-    });
-  });
-  
 });
 
 
