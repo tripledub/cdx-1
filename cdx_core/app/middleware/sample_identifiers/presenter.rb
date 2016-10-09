c097bbdc0fac4adf6ff3dbcb58f2f175d95aa839
Jorge Alvarez <jorge@alvareznavarro.es>
2016-10-09 13:37:43 +0200

    Find first sample available.

--- cdx_core/app/middleware/sample_identifiers/presenter.rb
+++ cdx_core/app/middleware/sample_identifiers/presenter.rb
@@ -3,10 +3,7 @@ module SampleIdentifiers
   class Presenter
     class << self
       def available_samples(encounter)
-        assigned_samples = encounter.patient_results.map(&:sample_identifier_id).compact
-        assigned_samples << 0 if assigned_samples.empty?
-        SampleIdentifier.joins(:sample).where('samples.encounter_id = ?', encounter.id)
-                        .where('sample_identifiers.id NOT IN(?)', assigned_samples).pluck(:lab_sample_id, :id)
+        SampleIdentifier.joins(:sample).where('samples.encounter_id = ?', encounter.id).pluck(:lab_sample_id, :id)
       end
     end
   end
