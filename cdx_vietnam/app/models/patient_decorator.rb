f42a74acca62ea9bf4cd5bab726c7ad5e3933f68
Stewart Campbell <stewartcuk@gmail.com>
2016-10-11 18:42:09 +0100

    Adds external system as drop-down

--- cdx_vietnam/app/models/patient_decorator.rb
+++ cdx_vietnam/app/models/patient_decorator.rb
@@ -3,7 +3,7 @@ class Patient < ActiveRecord::Base
   validates :social_security_code, length: { in: 9..15 }
 
   def display_patient_id
-    'VPN' << self.id.to_s.rjust(6,'0')
+    'VPN' << id.to_s.rjust(6,'0')
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
-
+if defined? CdxVietnam
+  [
+    { prefix: 'nlh', name: 'NLH Production HIS' },
+    { prefix: 'hlh', name: 'HLH Production HIS' },
+    { prefix: 'vit', name: 'Vitimes' },
+    { prefix: 'etb', name: 'Production e-TB Manager' }
+  ].each do |system|
+    extsys = ExternalSystem.where(prefix: system[:prefix]).first_or_create
+    extsys.update_attributes(system)
+  end
+end
