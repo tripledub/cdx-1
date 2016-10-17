58a01f399ce6c46ae6a5d64c31537e96a3f5016d
Jorge Alvarez <jorge@alvareznavarro.es>
2016-10-17 16:42:17 +0200

    Avoid patient ssc validation if users selects so.

--- cdx_vietnam/spec/models/patient_spec.rb
+++ cdx_vietnam/spec/models/patient_spec.rb
@@ -1,21 +1,44 @@
 require 'spec_helper'
 
 RSpec.describe Patient, type: :model do
+  let(:patient) { Patient.make }
 
   describe '#social_security_code' do
+    context 'should be validated' do
+      it 'is a string between 9 and 15 chars' do
+        patient.social_security_code = SecureRandom.hex(6)
+        expect(patient).to be_valid
+      end
 
-    context 'is a 10 character string' do
-      let(:patient) { Patient.make }
+      it 'is a string with less than 9 characters' do
+        patient.social_security_code = SecureRandom.hex(3)
+        expect(patient).to be_invalid
+      end
 
-      it { expect(patient).to be_valid }
+      it 'is a string with less more than 15 characters' do
+        patient.social_security_code = SecureRandom.hex(16)
+        expect(patient).to be_invalid
+      end
     end
 
-    context 'is a 4 character string' do
-      let(:patient) { Patient.make }
+    context 'should not be validated' do
+      before :each do
+        patient.skip_ssc_validation = true
+      end
 
-      it do
-        patient.social_security_code = 1234
-        expect(patient).to be_invalid
+      it 'is a string between 9 and 15 chars' do
+        patient.social_security_code = SecureRandom.hex(6)
+        expect(patient).to be_valid
+      end
+
+      it 'is a string with less than 9 characters' do
+        patient.social_security_code = SecureRandom.hex(3)
+        expect(patient).to be_valid
+      end
+
+      it 'is a string with less more than 15 characters' do
+        patient.social_security_code = SecureRandom.hex(16)
+        expect(patient).to be_valid
       end
     end
   end
