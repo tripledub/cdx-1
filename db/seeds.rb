eb072948067307d4e4bc6f2d5ea2fc8043c8dbe9
Shaun Dobie <shaun.dobie@bluefrontier.local>
2016-10-24 16:19:35 +0100

    2553, 2550, 2498, 2355 - changes to patients dropdown, episode, and results formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,9 @@ if defined? CdxVietnam
   [
     { prefix: 'nlh', name: 'NLH Production HIS' },
     { prefix: 'hlh', name: 'HLH Production HIS' },
+    { prefix: 'log', name: 'Log Book' },
     { prefix: 'vit', name: 'Vitimes' },
-    { prefix: 'etb', name: 'Production e-TB Manager' }
+    { prefix: 'etb', name: 'E-TB Manager' }
   ].each do |system|
     extsys = ExternalSystem.where(prefix: system[:prefix]).first_or_create
     extsys.update_attributes(system)
