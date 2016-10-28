4583f194d5d5c555dd15d704a3b42fbda78a7fda
Jorge Alvarez <jorge@alvareznavarro.es>
2016-10-28 14:30:59 +0200

    Update names to right ones.

--- cdx_vietnam/app/assets/javascripts/components/patients/show/history/patient_test_order.js.jsx
+++ cdx_vietnam/app/assets/javascripts/components/patients/show/history/patient_test_order.js.jsx
@@ -6,10 +6,10 @@ var PatientTestOrder = React.createClass({
   render: function(){
     return (
       <tr onClick={this.navigateTo.bind(this, this.props.patientTestOrder.viewLink)}>
-        <td>{this.props.patientTestOrder.siteName}</td>
+        <td>{this.props.patientTestOrder.requestedSiteName}</td>
         <td>{this.props.patientTestOrder.performingSiteName}</td>
         <td>{this.props.patientTestOrder.batchId}</td>
-        <td>{this.props.patientTestOrder.requester}</td>
+        <td>{this.props.patientTestOrder.requestedBy}</td>
         <td>{this.props.patientTestOrder.requestDate}</td>
         <td>{this.props.patientTestOrder.status}</td>
       </tr>
