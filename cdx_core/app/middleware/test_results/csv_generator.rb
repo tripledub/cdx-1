6b7c5c33eb77b1ce3fc0a3566258ef82e3d621a5
Jorge Alvarez <jorge@alvareznavarro.es>
2016-11-09 13:00:50 +0100

    Add csv export to presenter.

--- cdx_core/app/middleware/test_results/csv_generator.rb
+++ cdx_core/app/middleware/test_results/csv_generator.rb
@@ -3,12 +3,10 @@ module TestResults
   class CsvGenerator
     attr_reader :filename
 
-    def initialize(selected_tab, params, current_user, navigation_context, localization_helper)
+    def initialize(selected_tab, params, navigation_context)
       @selected_tab = selected_tab
       @params = params
-      @current_user = current_user
       @navigation_context = navigation_context
-      @localization_helper = localization_helper
       @filename = "#{selected_tab}_results-#{DateTime.now.strftime('%Y-%m-%d-%H-%M-%S')}.csv"
     end
 
@@ -23,14 +21,14 @@ module TestResults
       when 'dst_lpa'
         DstLpaResults::Presenter.csv_query(generate(::Finder::DstLpaResults))
       else
-        ::Finder::TestResults.new(@params, @current_user, @navigation_context, @localization_helper).csv_query(@filename)
+        TestResults::Presenter.csv_query(generate(::TestResults::Finder))
       end
     end
 
     protected
 
     def generate(results_finder)
-      patient_results = results_finder.new(@params, @navigation_context)
+      patient_results = results_finder.new(@navigation_context, @params)
       order_by = @params['order_by'] || 'patient_results.sample_collected_at desc'
       patient_results.filter_query.order(order_by)
     end
