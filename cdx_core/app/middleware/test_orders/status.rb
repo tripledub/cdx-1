83b60795e5fdbcf5eca93800c8457b94b122d3cd
Jorge Alvarez <jorge@alvareznavarro.es>
2016-11-29 16:46:27 +0100

    Return after update log.

--- cdx_core/app/middleware/test_orders/status.rb
+++ cdx_core/app/middleware/test_orders/status.rb
@@ -8,15 +8,15 @@ module TestOrders
         return if encounter.not_financed?
 
         if all_finished?(encounter)
-          update_and_log(encounter, 'closed')
+          return update_and_log(encounter, 'closed')
         elsif any_pending_approval?(encounter)
-          update_and_log(encounter, 'in_progress')
+          return update_and_log(encounter, 'in_progress')
         elsif any_sample_received?(encounter)
-          update_and_log(encounter, 'samples_received')
+          return update_and_log(encounter, 'samples_received')
         elsif any_sample_collected?(encounter)
-          update_and_log(encounter, 'samples_collected')
+          return update_and_log(encounter, 'samples_collected')
         elsif order_is_pending?(encounter)
-          update_and_log(encounter, 'pending')
+          return update_and_log(encounter, 'pending')
         end
       end
 
