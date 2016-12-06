65bc7970d9139cd1481312d2d1783ec84a453780
Jorge Alvarez <jorge@alvareznavarro.es>
2016-12-06 15:50:51 +0100

    Rename device timezone in users.

--- cdx_core/db/migrate/20161206095144_add_time_zone_to_sites.rb
+++ cdx_core/db/migrate/20161206095144_add_time_zone_to_sites.rb
@@ -2,5 +2,6 @@
 class AddTimeZoneToSites < ActiveRecord::Migration
   def change
     add_column :sites, :time_zone, :string, default: 'UTC'
+    rename_column :users, :timestamps_in_device_time_zone, :timestamps_in_site_time_zone
   end
 end
