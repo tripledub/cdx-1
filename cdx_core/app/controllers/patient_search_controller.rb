4d1474dd66d22ea757d86c68d55ebe317090860e
Jorge Alvarez <jorge@alvareznavarro.es>
2016-12-08 12:27:22 +0100

    Remove decent exposure.

--- cdx_core/app/controllers/patient_search_controller.rb
+++ cdx_core/app/controllers/patient_search_controller.rb
@@ -1,9 +1,10 @@
+# Returns a json with patients based
 class PatientSearchController < ApplicationController
   respond_to :json
 
-  expose(:patients) { Patient.all }
-
   def index
-    render json: Patients::Presenter.search_results(patients.where(patients.arel_table[:name].matches("%#{params[:q]}%")).order(:name).limit(30))
+    render json: Patients::Presenter.search_results(
+      Patient.where(Patient.arel_table[:name].matches("%#{params[:q]}%")).order(:name).limit(30)
+    )
   end
 end
