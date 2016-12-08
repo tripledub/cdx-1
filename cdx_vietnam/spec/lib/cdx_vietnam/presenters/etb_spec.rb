6eeac4e77a7097a5b62f325e7d0e6fac9d98be0c
Nghi P <nghipm@elarion.com>
2016-12-08 09:47:34 +0700

    update spec for vtm and etb json

--- cdx_vietnam/spec/lib/cdx_vietnam/presenters/etb_spec.rb
+++ cdx_vietnam/spec/lib/cdx_vietnam/presenters/etb_spec.rb
@@ -7,26 +7,60 @@ describe CdxVietnam::Presenters::Etb do
   let(:patient)             { Patient.make institution: institution, gender: 'male' }
   let(:encounter)           { Encounter.make institution: institution, user: current_user, patient: patient }
   let(:xpert_result)        { XpertResult.make encounter: encounter, tuberculosis: 'indeterminate', result_at: 3.days.ago }
+  let(:patient2)            { Patient.make institution: institution, gender: 'male', nationality: 'native' }
+  let(:encounter2)          { Encounter.make institution: institution, user: current_user, patient: patient2 }
+  let(:microscopy_result)   { MicroscopyResult.make encounter: encounter2, result_at: 1.day.ago }
 
-  context "must" do
-    before(:each) do
+  context "xpert result must" do
+    before(:example) do
       episode = patient.episodes.make
+      @json = CdxVietnam::Presenters::Etb.create_patient(xpert_result)
     end
-    
+
     it "responds with JSON string" do
-      json = described_class.create_patient(xpert_result)
-      expect(check_json_string(json)).to eq(true)
+      expect(check_json_string(@json)).to eq(true)
     end
 
     it "have patient field and test_order, target_system in patient field" do
-      json = described_class.create_patient(xpert_result)
-      json = JSON.parse(json)
-      patient_field = json['patient']
+      jsonx = JSON.parse(@json)
+      patient_field = jsonx['patient']
       expect(patient_field.nil?).to eq(false)
       expect(patient_field['test_order'].nil?).to eq(false)
       expect(patient_field['target_system'].nil?).to eq(false)
     end
   end
+
+  context "microscopy result must" do
+    before(:example) do
+      episode = patient2.episodes.make
+      @json = CdxVietnam::Presenters::Etb.create_patient(microscopy_result)
+    end
+    
+    it "responds with JSON string" do
+      expect(check_json_string(@json)).to eq(true)
+    end
+
+    context "have" do
+      before(:example) do
+        json = JSON.parse(@json)
+        @patient_field = json['patient']
+      end
+
+      it "patient field and test_order, target_system in patient field" do
+        expect(@patient_field.nil?).to eq(false)
+      end
+
+      it "test order field in patient field" do
+        expect(@patient_field['test_order'].nil?).to eq(false)
+      end
+
+      it "target system field in patient field" do
+        expect(@patient_field['target_system'].nil?).to eq(false)
+      end
+    end
+    
+  end
+
 end
 
 def check_json_string(json_string)
--- cdx_vietnam/spec/lib/cdx_vietnam/presenters/vtm_spec.rb
+++ cdx_vietnam/spec/lib/cdx_vietnam/presenters/vtm_spec.rb
@@ -6,10 +6,13 @@ describe CdxVietnam::Presenters::Vtm do
   let(:site)                { Site.make(institution: institution) }
   let(:patient)             { Patient.make institution: institution, gender: 'male' }
   let(:encounter)           { Encounter.make institution: institution, user: current_user, patient: patient }
-  let(:microscopy_result)   { MicroscopyResult.make encounter: encounter, result_at: 1.day.ago }
+  let(:microscopy_result)   { MicroscopyResult.make encounter: encounter, result_at: 1.day.ago, result_name: 'microscopy' }
+  let(:patient2)            { Patient.make institution: institution, gender: 'male', nationality: 'native' }
+  let(:encounter2)          { Encounter.make institution: institution, user: current_user, patient: patient2 }
+  let(:xpert_result)        { XpertResult.make encounter: encounter2, tuberculosis: 'indeterminate', result_at: 3.days.ago, result_name: 'xpertmtb' }
 
 
-  context "must" do
+  context "microscopy result must" do
     it "responds with JSON string" do
       episode = patient.episodes.make
       json = CdxVietnam::Presenters::Vtm.create_patient(microscopy_result)
@@ -23,9 +26,32 @@ describe CdxVietnam::Presenters::Vtm do
       patient_field = json['patient']
       expect(patient_field.nil?).to eq(false)
       expect(patient_field['test_order'].nil?).to eq(false)
+      test_order = patient_field['test_order']
+      expect(test_order['type'].to_s).to eq('microscopy')
       expect(patient_field['target_system'].nil?).to eq(false)
     end
   end
+
+  context "xpert result must" do
+    it "responds with JSON string" do
+      episode = patient.episodes.make
+      json = CdxVietnam::Presenters::Vtm.create_patient(xpert_result)
+      expect(check_json_string(json)).to eq(true)
+    end
+
+    it "have patient field and test_order, target_system in patient field" do
+      episode = patient.episodes.make
+      json = CdxVietnam::Presenters::Vtm.create_patient(xpert_result)
+      json = JSON.parse(json)
+      patient_field = json['patient']
+      expect(patient_field.nil?).to eq(false)
+      expect(patient_field['test_order'].nil?).to eq(false)
+      test_order = patient_field['test_order']
+      expect(test_order['type'].to_s).to eq('xpert')
+      expect(patient_field['target_system'].nil?).to eq(false)
+    end
+  end
+
 end
 
 def check_json_string(json_string)
