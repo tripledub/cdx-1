25c0e7fbb3a87266c1c01ebb09da2af48fd65fca
Jorge Alvarez <jorge@alvareznavarro.es>
2016-12-09 15:27:50 +0100

    Disable button when clicked.

--- cdx_core/app/assets/javascripts/components/encounters/new/footer.js.jsx
+++ cdx_core/app/assets/javascripts/components/encounters/new/footer.js.jsx
@@ -1,15 +1,24 @@
 class EncounterFooter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { btnDisabled: false };
+  }
+
   validateThenSave(event) {
     event.preventDefault();
-    this.props.validateThenSave();
+    this.setState({ btnDisabled: true });
+    if (this.props.validateThenSave() === false) {
+      this.setState({ btnDisabled: false });
+    };
   }
+
   render(){
     return(
       <RowContainer classNames="labelfooter">
         <div className="col-12">
           <ul>
             <li>
-              <a href="#" id="encountersave" className="button save" onClick={ this.validateThenSave.bind(this) }>{ I18n.t("components.fresh_tests_encounter_form.save_btn") }</a>
+              <button className="btn-primary save" disabled={ this.state.btnDisabled } onClick={ this.validateThenSave.bind(this) }>{ I18n.t("components.fresh_tests_encounter_form.save_btn") }</button>
             </li>
             <li>
               <a href={ this.props.cancelUrl } className="button cancel">{ I18n.t("components.fresh_tests_encounter_form.cancel_btn") }</a>
